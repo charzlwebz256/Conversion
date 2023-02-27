396c2121eea60f1bacd9381eab4bef6bf0b66348
charzlwebz256 <charzlwebz256@gmail.com>
2023-02-27 17:13:00 +0300

    Done

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ function KmtMetre( ){
     console.log("8km are equal to", metre, "m"); 
 }
 KmtMetre();
+
+function MetreTcent() {
+    let metre = 5;
+    let cm = metre/100;
+    console.log("5m are equal to", cm, "cm");
+}
+MetreTcent();
+
+function nanoTmm() {
+    let nano = 6;
+    let mm = nano/1000000;
+    console.log("6nano are equal to", mm, "mm");
+}
+nanoTmm();
+
+function dollarTKsh() {
+    let dollar = 1000;
+    let ksh = 1000*108;
+     let balance = ksh-700;
+    console.log("The balance is ", balance, "ksh");
+}
+dollarTKsh();
