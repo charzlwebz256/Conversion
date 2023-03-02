81355782c35e0cddddf2b7f8e6e97addec8e2ed3
charzlwebz256 <charzlwebz256@gmail.com>
2023-03-02 16:43:54 +0300

    Arrays

---
+++ arrays.js
@@ -0,0 +1,19 @@
+// Example 1
+var username = "Charz";
+console.log("username:", username);
+
+// Example 2
+let usernames = new Array("Lwebz", "Charz", "Tom", "Sam");
+console.log(usernames);
+
+console.log(usernames.length);
+usernames.push("Scovia")
+console.log(usernames);
+
+usernames.pop();
+console.log(usernames);
+
+usernames.shift();
+console.log(usernames);
+
+console.log(usernames[2]);
