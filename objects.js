3b4901b3e7f092c15667ebaa7479a9d1fb286557
charzlwebz256 <charzlwebz256@gmail.com>
2023-03-02 15:35:48 +0300

    Complete

--- objects.js
+++ objects.js
@@ -5,4 +5,5 @@ const person = {
     Occupation: "programmer",
     Maritalstatus :"single",
 }
+person.name = "David";
 console.log("His name is " + person.name + " and  he lives in " + person.location +" where he works as a " + person.Occupation + " and he is " + person.Maritalstatus + " for your case.");
